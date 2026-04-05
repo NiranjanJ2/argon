55fdd1cbbf6e074f8faa5e0d231af0b9b8e9e338
NiranjanJ2 <mail2niranjanj@gmail.com>
2026-04-04 17:44:48 -0700

    fix(google): update classroom scope to student-submissions.me.readonly

--- nanobot/google/auth.py
+++ nanobot/google/auth.py
@@ -37,7 +37,7 @@ ACCOUNT_SCOPES: dict[str, list[str]] = {
     ],
     "school": [
         "https://www.googleapis.com/auth/classroom.courses.readonly",
-        "https://www.googleapis.com/auth/classroom.coursework.me.readonly",
+        "https://www.googleapis.com/auth/classroom.student-submissions.me.readonly",
         "https://www.googleapis.com/auth/classroom.announcements.readonly",
         "https://www.googleapis.com/auth/classroom.rosters.readonly",
         "https://www.googleapis.com/auth/drive.readonly",
